aa3744b8e15c0d36633809eabc47e880127b2840
vainawebm <vainaweb@gmail.com>
2020-07-27 16:35:19 -0300

    novos arquivos criados

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+let fruit = 36;
+
+/* 1- Crie uma função que receba um número e retorne o dobro dele */
+
+const getMess = (fruit) => {
+    return fruit * 2;
+};
+
+console.log();
+
+/* 2 - Crie uma função que receba sua altura e peso e retorne o IMC */
+
+/* 3 - Crie uma função que receba seu ano de nascimento e retorne sua idade */
+
+/* 4 - Crie uma função que receba um Array com o nome de seus amigos
+  e retorne uma mensagem: "Meus melhores amigos são "kraken", "kelvin", "rhuan". */
+
+/* 5 - Crie uma função que receba o dia e o mês do seu aniversário e retorne
+  seu signo :) */
